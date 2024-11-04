82a1f4a21da480e1dff9e2ea832e82681fc7d4da
João Paulo Alves Campos <152233739+GATOROXO@users.noreply.github.com>
2024-11-04 09:33:31 -0300

    Update bullets(SpaceX) -- 0.0.2 04/11/2024 9:32

--- bullets.py
+++ bullets.py
@@ -9,16 +9,17 @@ class bullet(pygame.sprite.Sprite):
         # Carrega a imagem do missil
         bullet_image_path = os.path.join(os.path.dirname(__file__), 'bullet.png')
         self.surf = pygame.image.load(bullet_image_path).convert_alpha()  # Usa a imagem
-        self.surf = pygame.transform.scale(self.surf, (20, 20))
+        self.surf = pygame.transform.rotate(self.surf, 90)  # Rotaciona a imagem
+        self.surf = pygame.transform.scale(self.surf, (40, 40))
 
         # Inicializa a posição do missil de acordo com a posiçao do player
-        self.rect = self.surf.get_rect(
-            center=(player.rect.centerx, player.rect.centery)
-        )
-        self.speed = 30
+        self.rect = self.surf.get_rect(center=(player.rect.centerx, player.rect.centery))
+        self.hitbox =pygame.Rect(self.rect.x + 10, self.rect.y + 10, 20, 20)
+        self.speed = 15
 
     def update(self): 
         self.rect.move_ip(self.speed, 0)  # Move o missil para a direita
+        self.hitbox.topleft = self.rect.topleft
         if self.rect.right > 800:  # Verifica se o missil saiu da tela
             self.kill()  # Remove o missil
 
