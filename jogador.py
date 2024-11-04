c897d3965dd96c8561720755448cf99ea422b18a
João Paulo Alves Campos <152233739+GATOROXO@users.noreply.github.com>
2024-11-04 13:54:58 -0300

    Update Jogador(SpaceX) -- 0.0.3 04/11/2024 13:54

--- jogador.py
+++ jogador.py
@@ -8,11 +8,13 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         # Verifique o caminho correto para a imagem
-        image_path = os.path.join(os.path.dirname(__file__), 'spacecraft.png')
+        image_path = os.path.join(os.path.dirname(__file__), 'foguete.png')
         self.surf = pygame.image.load(image_path).convert_alpha()  # Carrega a imagem da nave
-        self.surf = pygame.transform.scale(self.surf, (15, 50))  # Redimensiona a imagem da nave
-        self.surf = pygame.transform.rotate(self.surf, -90)  # Rotaciona a imagem 90 graus à direita
+        self.surf = pygame.transform.scale(self.surf, (100, 100))  # Redimensiona a imagem da nave
+        #self.rect = self.surf.get_rect(center=(400, 300))
         self.rect = self.surf.get_rect(center=(400, 300))  # Inicializa no centro da tela
+        self.hitbox = pygame.Rect(self.rect.x + 10, self.rect.y + 10,self.rect.width - 55, self.rect.height - 75) # Inicializa a hitbox da nave
+        #self.surf = pygame.transform.rotate(self.surf, -90)  # Rotaciona a imagem 90 graus à direita
         self.speed = 5
 
     # Atualiza a posição do jogador conforme as teclas pressionadas
@@ -30,6 +32,9 @@ class Player(pygame.sprite.Sprite):
             self.rect.move_ip(-self.speed, 0)
         if pressed_keys[K_d]:
             self.rect.move_ip(self.speed, 0)
+
+        # Atualiza a hitbox da nave
+        self.hitbox.topleft = ( self.rect.x + 30, self.rect.y + 40 )
         
         # Limites laterais (sem wrap-around)
         if self.rect.left < 0:  # Não permitir sair pela esquerda
@@ -42,3 +47,5 @@ class Player(pygame.sprite.Sprite):
             self.rect.bottom = 0  # Coloca na parte superior
         elif self.rect.bottom < 0:  # Sai pela parte superior
             self.rect.top = 600  # Coloca na parte inferior
+
+
